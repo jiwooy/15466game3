2b1679191aba4056191045269f440963c6f5ab43
jiwooy <jiwyoo629@gmail.com>
2020-09-27 22:56:39 -0400

    maze creation, collisions, play sounds

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -14,13 +14,13 @@
 
 GLuint hexapod_meshes_for_lit_color_texture_program = 0;
 Load< MeshBuffer > hexapod_meshes(LoadTagDefault, []() -> MeshBuffer const * {
-	MeshBuffer const *ret = new MeshBuffer(data_path("hexapod.pnct"));
+	MeshBuffer const *ret = new MeshBuffer(data_path("maze.pnct"));
 	hexapod_meshes_for_lit_color_texture_program = ret->make_vao_for_program(lit_color_texture_program->program);
 	return ret;
 });
 
 Load< Scene > hexapod_scene(LoadTagDefault, []() -> Scene const * {
-	return new Scene(data_path("hexapod.scene"), [&](Scene &scene, Scene::Transform *transform, std::string const &mesh_name){
+	return new Scene(data_path("maze.scene"), [&](Scene &scene, Scene::Transform *transform, std::string const &mesh_name){
 		Mesh const &mesh = hexapod_meshes->lookup(mesh_name);
 
 		scene.drawables.emplace_back(transform);
@@ -36,24 +36,92 @@ Load< Scene > hexapod_scene(LoadTagDefault, []() -> Scene const * {
 	});
 });
 
-Load< Sound::Sample > dusty_floor_sample(LoadTagDefault, []() -> Sound::Sample const * {
-	return new Sound::Sample(data_path("dusty-floor.opus"));
+Load< Sound::Sample > wind_chimes(LoadTagDefault, []() -> Sound::Sample const * {
+	return new Sound::Sample(data_path("wind_chimes.wav"));
 });
+
+Load< Sound::Sample > tired(LoadTagDefault, []() -> Sound::Sample const * {
+	return new Sound::Sample(data_path("tired.wav"));
+});
+
+Load< Sound::Sample > gen_sound(LoadTagDefault, []() -> Sound::Sample const * {
+	return new Sound::Sample(data_path("generator.wav"));
+});
+
+Load< Sound::Sample > got_item(LoadTagDefault, []() -> Sound::Sample const * {
+	return new Sound::Sample(data_path("got_item.wav"));
+});
+
+void PlayMode::init_maze(int setting) {
+	printf("init maze... ");
+	uint8_t arr[10][10] {
+	{0, 2, 1, 0, 1, 1, 2, 1, 1, 1},
+	{0, 1, 1, 0, 1, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 1, 0, 1, 0, 1, 0},
+	{1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
+	{2, 1, 0, 1, 0, 2, 1, 2, 0, 0},
+	{0, 1, 0, 0, 0, 1, 1, 1, 1, 0},
+	{0, 1, 0, 1, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 1, 0, 1, 1, 1, 0, 1},
+	{0, 1, 1, 1, 0, 1, 2, 1, 0, 1},
+	{0, 1, 0, 0, 0, 1, 0, 0, 0, 0},
+	};
+	uint8_t size = 0;
+	uint8_t size2 = 0;
+	for (uint8_t i = 0; i < 10; i++) {
+		for (uint8_t j = 0; j < 10; j++) {
+			float xpos = 10.0f * j - 45.0f;
+			float ypos = -10.0f * i + 45.0f;
+			if (arr[i][j] == 1 && setting == 1) {
+				//printf("iter %d %d %s", i, j, block->transform->name.c_str());
+				size++;
+				Scene::Drawable *new_block = new Scene::Drawable(*block);
+				Scene::Transform *t = new Scene::Transform;
+				t->rotation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+				t->position = glm::vec3(xpos, ypos, 6.0f);
+				t->scale = block->transform->scale;
+				t->name = "Block" + std::to_string(size);
+				t->parent = block->transform->parent;
+				scene.transforms.emplace_back(*t);
+				new_block->transform = t;
+				scene.drawables.emplace_back(*new_block);
+				blocks.push_back(new_block);
+			}
+			if (arr[i][j] == 2 && setting == 2) {
+				size2++;
+				Scene::Drawable *new_gen = new Scene::Drawable(*generator);
+				Scene::Transform *t = new Scene::Transform;
+				t->rotation = generator->transform->rotation;
+				t->position = glm::vec3(xpos, ypos, 3.0f);
+				t->scale = generator->transform->scale;
+				t->name = "Generator" + std::to_string(size);
+				t->parent = generator->transform->parent;
+				scene.transforms.emplace_back(*t);
+				new_gen->transform = t;
+				scene.drawables.emplace_back(*new_gen);
+				generators.push_back(new_gen);
+				std::shared_ptr< Sound::PlayingSample > gen = Sound::loop_3D(*gen_sound, 10.0f, t->position, 0.01f);
+				gen_sounds.push_back(gen);
+			}
+		}
+	}
+}
 
 PlayMode::PlayMode() : scene(*hexapod_scene) {
 	//get pointers to leg for convenience:
-	for (auto &transform : scene.transforms) {
-		if (transform.name == "Hip.FL") hip = &transform;
-		else if (transform.name == "UpperLeg.FL") upper_leg = &transform;
-		else if (transform.name == "LowerLeg.FL") lower_leg = &transform;
+	for (auto drawable : scene.drawables) {
+		if (drawable.transform->name == "Sphere") {
+			player = drawable.transform;
+		} else if (drawable.transform->name == "Block") {
+			block = &drawable;
+			init_maze(1);
+		} else if (drawable.transform->name == "Generator") {
+			generator = &drawable;
+			init_maze(2);
+		} else if (drawable.transform->name == "Cylinder") {
+			enemy = drawable.transform;
+		}
 	}
-	if (hip == nullptr) throw std::runtime_error("Hip not found.");
-	if (upper_leg == nullptr) throw std::runtime_error("Upper leg not found.");
-	if (lower_leg == nullptr) throw std::runtime_error("Lower leg not found.");
-
-	hip_base_rotation = hip->rotation;
-	upper_leg_base_rotation = upper_leg->rotation;
-	lower_leg_base_rotation = lower_leg->rotation;
 
 	//get pointer to camera for convenience:
 	if (scene.cameras.size() != 1) throw std::runtime_error("Expecting scene to have exactly one camera, but it has " + std::to_string(scene.cameras.size()));
@@ -61,7 +129,19 @@ PlayMode::PlayMode() : scene(*hexapod_scene) {
 
 	//start music loop playing:
 	// (note: position will be over-ridden in update())
-	leg_tip_loop = Sound::loop_3D(*dusty_floor_sample, 1.0f, get_leg_tip_position(), 10.0f);
+	enemy_sound = Sound::loop_3D(*wind_chimes, 15.0f, enemy->position, 0.05f);
+	glm::quat rot = rotate(camera->transform->rotation, -3.18f ,glm::vec3(0,0,1));
+	//printf("%f %f %f %f\n", camera->transform->rotation[0], camera->transform->rotation[1], camera->transform->rotation[2], camera->transform->rotation[3]);
+	camera->transform->rotation = rot;
+	rot = rotate(camera->transform->rotation, -3.3f ,glm::vec3(0,1,0));
+	camera->transform->rotation = rot;
+
+	camera->transform->rotation[0] = -0.002011f;
+	camera->transform->rotation[1] = -0.003080f;
+	camera->transform->rotation[2] = 0.001323f; 
+	camera->transform->rotation[3] = 0.999992f;
+	camera->transform->position = glm::vec3(player->position[0], player->position[1], 70.0f);
+	
 }
 
 PlayMode::~PlayMode() {
@@ -89,6 +169,10 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 			down.downs += 1;
 			down.pressed = true;
 			return true;
+		} else if (evt.key.keysym.sym == SDLK_SPACE) {
+			space.downs += 1;
+			space.pressed = true;
+			return true;
 		}
 	} else if (evt.type == SDL_KEYUP) {
 		if (evt.key.keysym.sym == SDLK_a) {
@@ -102,6 +186,9 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 			return true;
 		} else if (evt.key.keysym.sym == SDLK_s) {
 			down.pressed = false;
+			return true;
+		} else if (evt.key.keysym.sym == SDLK_SPACE) {
+			space.pressed = false;
 			return true;
 		}
 	} else if (evt.type == SDL_MOUSEBUTTONDOWN) {
@@ -129,53 +216,65 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 
 void PlayMode::update(float elapsed) {
 
-	//slowly rotates through [0,1):
-	wobble += elapsed / 10.0f;
-	wobble -= std::floor(wobble);
-
-	hip->rotation = hip_base_rotation * glm::angleAxis(
-		glm::radians(5.0f * std::sin(wobble * 2.0f * float(M_PI))),
-		glm::vec3(0.0f, 1.0f, 0.0f)
-	);
-	upper_leg->rotation = upper_leg_base_rotation * glm::angleAxis(
-		glm::radians(7.0f * std::sin(wobble * 2.0f * 2.0f * float(M_PI))),
-		glm::vec3(0.0f, 0.0f, 1.0f)
-	);
-	lower_leg->rotation = lower_leg_base_rotation * glm::angleAxis(
-		glm::radians(10.0f * std::sin(wobble * 3.0f * 2.0f * float(M_PI))),
-		glm::vec3(0.0f, 0.0f, 1.0f)
-	);
-
-	//move sound to follow leg tip position:
-	leg_tip_loop->set_position(get_leg_tip_position(), 1.0f / 60.0f);
-
 	//move camera:
 	{
 
 		//combine inputs into a move:
-		constexpr float PlayerSpeed = 30.0f;
+		float PlayerSpeed = 8.0f;
 		glm::vec2 move = glm::vec2(0.0f);
-		if (left.pressed && !right.pressed) move.x =-1.0f;
-		if (!left.pressed && right.pressed) move.x = 1.0f;
-		if (down.pressed && !up.pressed) move.y =-1.0f;
-		if (!down.pressed && up.pressed) move.y = 1.0f;
+		if (left.pressed && !right.pressed) move.x =-0.5f;
+		if (!left.pressed && right.pressed) move.x = 0.5f;
+		if (down.pressed && !up.pressed) move.y =-0.5f;
+		if (!down.pressed && up.pressed) move.y = 0.5f;
+		if (space.pressed) {
+			if (sprint > 0.0f) {
+				PlayerSpeed *= 2.0f;
+				sprint -= 0.75f;
+				if (sprint <= 0.0f) {
+					penalty = true;
+				}
+			}
+		} else {
+			if (!penalty) {
+				sprint = std::min(100.0f, sprint + 0.5f);
+			}
+			else {
+				recovery_timer += elapsed;
+				if (recovery_timer >= 5.0f) {
+					recovery_timer = 0.0f;
+					penalty = false;
+				}
+			}
+		}
+		if (sprint <= 15.0f) {
+			if (tired_sound != nullptr && tired_sound.get()->stopped) tired_sound = Sound::play(*tired, 0.7f, 0.0f);
+			if (tired_sound == nullptr) tired_sound = Sound::play(*tired, 0.7f, 0.0f);
+		}
 
 		//make it so that moving diagonally doesn't go faster:
 		if (move != glm::vec2(0.0f)) move = glm::normalize(move) * PlayerSpeed * elapsed;
+
 
 		glm::mat4x3 frame = camera->transform->make_local_to_parent();
 		glm::vec3 right = frame[0];
 		//glm::vec3 up = frame[1];
 		glm::vec3 forward = -frame[2];
-
-		camera->transform->position += move.x * right + move.y * forward;
+		//camera->transform->position += move.x * right + move.y * forward;
+		
+		if (block_bounds(move)) move_player(move);
+		wall_bounds();
+		camera->transform->position[0] = player->position[0];
+		camera->transform->position[1] = player->position[1];
+		obtain_generator();
 	}
 
 	{ //update listener to camera position:
-		glm::mat4x3 frame = camera->transform->make_local_to_parent();
+		glm::mat4x3 frame = player->make_local_to_parent();
 		glm::vec3 right = frame[0];
-		glm::vec3 at = frame[3];
-		Sound::listener.set_position_right(at, right, 1.0f / 60.0f);
+		//printf("right %f %f %f\n", right[0], right[1], right[2]);
+		Sound::listener.set_position_right(player->position, right, 1.0f / 60.0f);
+
+		enemy_sound.get()->set_position(enemy->position);
 	}
 
 	//reset button press counters:
@@ -185,19 +284,76 @@ void PlayMode::update(float elapsed) {
 	down.downs = 0;
 }
 
+void PlayMode::move_player(glm::vec2 move) {
+	player->position[0] += move.x;
+	player->position[1] += move.y;
+}
+
+void PlayMode::wall_bounds() {
+	if (player->position[0] + 1.5f > 50.0f) player->position[0] = 48.5;
+	if (player->position[0] - 1.5f < -50.0f) player->position[0] = -48.5;
+	if (player->position[1] + 1.5f > 50.0f) player->position[1] = 48.5;
+	if (player->position[1] - 1.5f < -50.0f) player->position[1] = -48.5;
+}
+
+bool PlayMode::block_bounds(glm::vec2 move) {
+	//printf("size %zd\n", blocks.size());
+	for (size_t i = 0; i < blocks.size(); i++) {
+		//printf("%zd %p\n", i, blocks[i]->transform);
+		if (std::max(player->position[0] - 1.5f + move.x, blocks[i]->transform->position[0] - 5.0f) <= 
+			std::min(player->position[0] + 1.5f + move.x, blocks[i]->transform->position[0] + 5.0f) &&
+			std::max(player->position[1] - 1.5f + move.y, blocks[i]->transform->position[1] - 5.0f) <= 
+			std::min(player->position[1] + 1.5f + move.y, blocks[i]->transform->position[1] + 5.0f)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+void PlayMode::obtain_generator() {
+	for (size_t i = 0; i < generators.size(); i++) {
+		if (std::max(player->position[0] - 1.5f, generators[i]->transform->position[0] - 1.5f) <= 
+			std::min(player->position[0] + 1.5f, generators[i]->transform->position[0] + 1.5f) &&
+			std::max(player->position[1] - 1.5f, generators[i]->transform->position[1] - 1.5f) <= 
+			std::min(player->position[1] + 1.5f, generators[i]->transform->position[1] + 1.5f)) {
+			generators[i]->transform->position[2] = -10;
+			generators.erase(generators.begin() + i);
+			gen_sounds[i].get()->stop();
+			gen_sounds.erase(gen_sounds.begin() + i);
+			got_generator = Sound::play(*got_item, 0.7f, 0.0f);
+		}
+	}
+}
+
+void PlayMode::player_die() {
+	if (std::max(player->position[0] - 1.5f, enemy->position[0] - 1.5f) <= 
+		std::min(player->position[0] + 1.5f, enemy->position[0] + 1.5f) &&
+		std::max(player->position[1] - 1.5f, enemy->position[1] - 1.5f) <= 
+		std::min(player->position[1] + 1.5f, enemy->position[1] + 1.5f)) {
+		die = true;
+	}
+}
+
 void PlayMode::draw(glm::uvec2 const &drawable_size) {
 	//update camera aspect ratio for drawable:
 	camera->aspect = float(drawable_size.x) / float(drawable_size.y);
 
 	//set up light type and position for lit_color_texture_program:
 	// TODO: consider using the Light(s) in the scene to do this
+	// glUseProgram(lit_color_texture_program->program);
+	// glUniform1i(lit_color_texture_program->LIGHT_TYPE_int, 2);
+	// glUniform3fv(lit_color_texture_program->LIGHT_LOCATION_vec3, 1,
+	// glm::value_ptr(glm::vec3(player->position[0], player->position[1], 20.0f)));
+	// glUniform3fv(lit_color_texture_program->LIGHT_DIRECTION_vec3, 1,
+	// glm::value_ptr(glm::vec3(0.0f, 0.0f,-1.0f)));
+	// glUniform1f(lit_color_texture_program->LIGHT_CUTOFF_float,
+	// std::cos(3.1415926f * 0.125f));
+	// glUniform3fv(lit_color_texture_program->LIGHT_ENERGY_vec3, 1,
+	// glm::value_ptr(200.0f * glm::vec3(1.0f, 1.0f, 0.95f)));
 	glUseProgram(lit_color_texture_program->program);
 	glUniform1i(lit_color_texture_program->LIGHT_TYPE_int, 1);
-	GL_ERRORS();
 	glUniform3fv(lit_color_texture_program->LIGHT_DIRECTION_vec3, 1, glm::value_ptr(glm::vec3(0.0f, 0.0f,-1.0f)));
-	GL_ERRORS();
 	glUniform3fv(lit_color_texture_program->LIGHT_ENERGY_vec3, 1, glm::value_ptr(glm::vec3(1.0f, 1.0f, 0.95f)));
-	GL_ERRORS();
 	glUseProgram(0);
 
 	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
@@ -230,9 +386,4 @@ void PlayMode::draw(glm::uvec2 const &drawable_size) {
 			glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
 			glm::u8vec4(0xff, 0xff, 0xff, 0x00));
 	}
-}
-
-glm::vec3 PlayMode::get_leg_tip_position() {
-	//the vertex position here was read from the model in blender:
-	return lower_leg->make_local_to_world() * glm::vec4(-1.26137f, -11.861f, 0.0f, 1.0f);
 }
--- PlayMode.hpp
+++ PlayMode.hpp
@@ -12,35 +12,46 @@ struct PlayMode : Mode {
 	PlayMode();
 	virtual ~PlayMode();
 
+	enum dir {udir, ddir, ldir, rdir};
+
 	//functions called by main loop:
 	virtual bool handle_event(SDL_Event const &, glm::uvec2 const &window_size) override;
 	virtual void update(float elapsed) override;
 	virtual void draw(glm::uvec2 const &drawable_size) override;
+	virtual void move_player(glm::vec2 move);
+	virtual void init_maze(int setting);
+	virtual void wall_bounds();
+	virtual bool block_bounds(glm::vec2 move);
+	virtual void obtain_generator();
+	virtual void player_die();
 
 	//----- game state -----
+	Scene::Transform *player = nullptr;
+	Scene::Transform *enemy = nullptr;
+	Scene::Drawable *block = nullptr;
+	Scene::Drawable *generator = nullptr;
+	float sprint = 100.0f;
+	float recovery_timer = 0.0f;
+	bool penalty = false;
+	bool die = false;
+
+	std::vector<Scene::Drawable *> blocks;
+	std::vector<Scene::Drawable *> generators;
 
 	//input tracking:
 	struct Button {
 		uint8_t downs = 0;
 		uint8_t pressed = 0;
-	} left, right, down, up;
+	} left, right, down, up, space;
 
 	//local copy of the game scene (so code can change it during gameplay):
 	Scene scene;
 
-	//hexapod leg to wobble:
-	Scene::Transform *hip = nullptr;
-	Scene::Transform *upper_leg = nullptr;
-	Scene::Transform *lower_leg = nullptr;
-	glm::quat hip_base_rotation;
-	glm::quat upper_leg_base_rotation;
-	glm::quat lower_leg_base_rotation;
-	float wobble = 0.0f;
-
-	glm::vec3 get_leg_tip_position();
-
 	//music coming from the tip of the leg (as a demonstration):
-	std::shared_ptr< Sound::PlayingSample > leg_tip_loop;
+	std::shared_ptr< Sound::PlayingSample > enemy_sound;
+	std::shared_ptr< Sound::PlayingSample > tired_sound = nullptr;
+	std::shared_ptr< Sound::PlayingSample > got_generator;
+	std::vector<std::shared_ptr< Sound::PlayingSample >> gen_sounds;
 	
 	//camera:
 	Scene::Camera *camera = nullptr;
